3329a899327242bceb0df638c858c2ea7692cfce
rohantandale1107 <rohanhtandale11@gmail.com>
2024-04-01 09:51:26 +0530

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'custom-black': '#0E0E0E',
+      },
+    },
+  },
+  plugins: [],
+}
+
